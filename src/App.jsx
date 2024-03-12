b3656827b39cc3f2facab8cc7fe8475887411e04
Kaic Machado <kaicmachado2@gmail.com>
2024-03-11 22:44:29 -0300

    estrutura inicial

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Login from "./Components/Login/Login";
+
+function App() {
+ return (
+  <div>
+   <BrowserRouter>
+    <Header />
+    <Routes>
+     <Route path="/" element={<Home />}></Route>
+     <Route path="/login" element={<Login />}></Route>
+    </Routes>
+    <Footer />
+   </BrowserRouter>
+  </div>
+ );
+}
+
+export default App;
