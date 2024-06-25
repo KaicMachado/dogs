a7132a4313e67313ebfb5ee51c96f230d42fbc56
Kaic Machado <kaicmachado2@gmail.com>
2024-06-24 21:49:06 -0300

    criado funcao de deletar foto e corrigido bug onde as informacoes da foto nao era enviado para a api

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -20,13 +20,14 @@ const UserPhotoPost = () => {
  useEffect(() => {
   if (data) navigate("./conta");
  }, [data, navigate]);
+
  function handleSubmit(e) {
   e.preventDefault();
   const formData = new FormData();
   formData.append("img", img.raw);
-  formData.append("nome", nome.raw);
-  formData.append("peso", peso.raw);
-  formData.append("idade", idade.raw);
+  formData.append("nome", nome.value);
+  formData.append("peso", peso.value);
+  formData.append("idade", idade.value);
 
   const token = window.localStorage.getItem("token");
   const { url, options } = PHOTO_POST(formData, token);
