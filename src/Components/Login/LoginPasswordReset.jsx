a6ab7f737e120111f537df00cc1342f175cd3bae
Kaic Machado <kaicmachado2@gmail.com>
2024-09-17 11:35:33 -0300

    criado secao de perdeu senha e funcionalidade para resetar senha

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import { PASSWORD_RESET } from "../../api";
+import Error from "../Helper/Error";
+import { useNavigate } from "react-router-dom";
 
 const LoginPasswordReset = () => {
- return <div>LoginPasswordReset</div>;
+ const [login, setLogin] = useState("");
+ const [key, setKey] = useState("");
+ const { loading, error, request } = useFetch();
+ const password = useForm("password");
+ const navigate = useNavigate();
+
+ useEffect(() => {
+  const params = new URLSearchParams(window.location.search);
+  const key = params.get("key");
+  const login = params.get("login");
+
+  if (key) setKey(key);
+  if (login) setLogin(login);
+ }, []);
+
+ async function handleSubmit(e) {
+  e.preventDefault();
+  if (password.validate()) {
+   const { url, options } = PASSWORD_RESET({
+    login,
+    key,
+    password: password.value,
+   });
+   const { response } = await request(url, options);
+   if (response.ok) navigate("./login");
+  }
+ }
+
+ return (
+  <div>
+   <h1 className="title">Resete a Senha</h1>
+   <form onSubmit={handleSubmit}>
+    <Input label="Nova Senha" type="password" name="password" {...password} />
+    {loading ? (
+     <Button disabled>Resetando...</Button>
+    ) : (
+     <Button>Resetar</Button>
+    )}
+   </form>
+   <Error error={error} />
+  </div>
+ );
 };
 
 export default LoginPasswordReset;
